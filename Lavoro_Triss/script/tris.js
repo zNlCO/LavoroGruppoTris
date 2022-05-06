029c4cbc37b14c6147040caf9f1cf580aa557d4d
Nico <87064940+zNlCO@users.noreply.github.com>
2022-05-06 13:17:32 +0200

    ciao

--- Lavoro_Triss/script/tris.js
+++ Lavoro_Triss/script/tris.js
@@ -1,1 +1,49 @@
-//*ciao
+/*
+<!DOCTYPE html>
+<html>
+
+<head>
+	<style>
+
+
+		#image {
+			display: none;
+		}
+	</style>
+</head>
+
+<body>
+	<div>
+		<h1>GeeksforGeeks</h1>
+		<h3>Click on the button to see image</h3>
+
+		<!-- Add id to image -->
+		<img id="image" src=
+"https://media.geeksforgeeks.org/wp-content/uploads/20210915115837/gfg3.png"
+			alt="GFG image" />
+	</div>
+
+	<button type="button"
+		onclick="show()" id="btnID">
+		Show Image
+	</button>
+
+	<script>
+		function show() {
+
+			document.getElementById('image')
+					.style.display = "block";
+
+			document.getElementById('btnID')
+					.style.display = "none";
+		}
+	</script>
+</body>
+
+</html>
+
+*/
+
+function mostraimmagine(){
+    
+}
