fb8e71d7da33151bd04067b358f05eb9b635a85d
Baddy2904 <battiatomatteo29@gmail.com>
2022-05-06 17:03:01 +0200

    divisione html / js

--- Lavoro_Triss/script/tris.js
+++ Lavoro_Triss/script/tris.js
@@ -1,54 +1,14 @@
-/*
-<!DOCTYPE html>
-<html>
+//primo metodo
+/*function mostraImmagine(){
+let image = document.getElementById("ImmagineTris");
 
-<head>
-	<style>
-
-
-		#image {
-			display: none;
-		}
-	</style>
-</head>
-
-<body>
-	<div>
-		<h1>GeeksforGeeks</h1>
-		<h3>Click on the button to see image</h3>
-
-		<!-- Add id to image -->
-		<img id="image" src=
-"https://media.geeksforgeeks.org/wp-content/uploads/20210915115837/gfg3.png"
-			alt="GFG image" />
-	</div>
-
-	<button type="button"
-		onclick="show()" id="btnID">
-		Show Image
-	</button>
-
-	<script>
-		function show() {
-
-			document.getElementById('image')
-					.style.display = "block";
-
-			document.getElementById('btnID')
-					.style.display = "none";
-		}
-	</script>
-</body>
-
-</html>
-
-*/
-
-/*function mostraimmagine()
-{
-    document.getElementById('ImmagineTris')
-    .style.display = "block";
-
-    document.getElementById('btnImg')
-    .style.display = "none";
+image.src = "../immagini/prova1.jpg"
+document.getElementById("btnImg").style.display = "none";
 }*/
+//secondo metodo
+function mostraImmagine()
+{
+	document.getElementById('ImmagineTris').style.display = "block";
+
+	document.getElementById('btnImg').style.display = "none";
+}
