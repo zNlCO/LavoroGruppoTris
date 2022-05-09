4d608c10eb34349fab110ab5b216200d2e879efa
Nico <nik04paolini@gmail.com>
2022-05-09 17:28:26 +0200

    fdsfs

--- Lavoro_Triss/script/mostra.js
+++ Lavoro_Triss/script/mostra.js
@@ -1,7 +1,1 @@
-function mostraImmagine()
-{
-	document.getElementById('board').style.display = "block";
-	document.getElementById('gioca_ancora').style.display = "block";
 
-	document.getElementById('btnImg').style.display = "none";
-}
