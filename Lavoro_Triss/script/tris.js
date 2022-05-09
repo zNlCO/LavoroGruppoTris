dcc32a7c3d143b831485edee35aafc8e7e62b383
Nico <nik04paolini@gmail.com>
2022-05-09 17:29:20 +0200

    tris

--- Lavoro_Triss/script/tris.js
+++ Lavoro_Triss/script/tris.js
@@ -87,11 +87,3 @@ return combination.every(index => {
 })
 })
 }
-
-function mostraImmagine()
-{
-	document.getElementById('board').style.display = "block";
-	document.getElementById('gioca_ancora').style.display = "block";
-
-	document.getElementById('btnImg').style.display = "none";
-}
