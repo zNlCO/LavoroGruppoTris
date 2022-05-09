194bdded15ca4be51e4483fd8183e8abb784ac32
Nico <nik04paolini@gmail.com>
2022-05-09 17:56:52 +0200

    gioca_ancora

--- Lavoro_Triss/script/mostra.js
+++ Lavoro_Triss/script/mostra.js
@@ -1,7 +1,7 @@
 function mostraImmagine()
 {
 	document.getElementById('board').style.display = "grid";
-	document.getElementById('gioca_ancora').style.display = "block";
+	document.getElementById('restartButton').style.display = "block";
 
 	document.getElementById('btnImg').style.display = "none";
 }
