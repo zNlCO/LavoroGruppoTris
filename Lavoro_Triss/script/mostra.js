192bcba3212d7a114fe968975d740243d51d5ea2
Baddy2904 <battiatomatteo29@gmail.com>
2022-05-11 17:17:08 +0200

    bloccaggio input

--- Lavoro_Triss/script/mostra.js
+++ Lavoro_Triss/script/mostra.js
@@ -1,7 +1,11 @@
 function mostraImmagine()
 {
+	//fa comparire il bottone gioca e il triss
 	document.getElementById('board').style.display = "grid";
 	document.getElementById('restartButton').style.display = "block";
-
+	//disabilita i bottoni (blocca quello che si inserisce)
+	document.getElementById('tfGioc1').disabled = true;
+	document.getElementById('tfGioc2').disabled = true;
+	//fa scomparire il bottone:GIOCA
 	document.getElementById('btnImg').style.display = "none";
 }
