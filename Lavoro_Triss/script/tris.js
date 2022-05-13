0e61889ebd184397d7299783525f298878880bf9
Nico <87064940+zNlCO@users.noreply.github.com>
2022-05-13 13:31:44 +0200

    cambiamento tris

--- Lavoro_Triss/script/tris.js
+++ Lavoro_Triss/script/tris.js
@@ -1,3 +1,4 @@
+/*
 const X_CLASS = 'x'
 const CIRCLE_CLASS = 'circle'
 const WINNING_COMBINATIONS = 
@@ -89,4 +90,67 @@ function checkWin(currentClass) {
 		return cellElements[index].classList.contains(currentClass)
 	})
 	})
+}
+*/
+
+var turno=0
+const comGiuste = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[2, 4, 6]
+  ]
+var arrayx = []
+var arrayy = []
+function assegna(posizione)
+{
+	if(turno%2==0)
+	{
+		document.images[posizione].src= "x.png"
+		arrayx.push(posizione)
+	}
+	else
+	{
+		document.images[posizione].src="o.png"
+		arrayy.push(posizione)
+	}
+	turno+=1
+
+	if(turno>4){
+		checkWin();
+	}
+}
+
+function checkWin(){
+	/*variabile per capire se c'è la casella esatta nella combinazione di vittoria*/
+	var casellaEsatta = false
+	//varibile per capire se raggiunge 3 caselle per "formare" la vittoria
+	var caselleEsatte = 0
+	//cicla le 8 combinazioni di vittoria del tris
+	for(var c = 0; i<8; c++){
+		//cicla le caselle dove c'è una x sopra
+		for(var i = 0; i<arrayx.length; i++){
+			//cicla una combinazione di vittoria 
+			for(var a = 0; a<3; a++){
+				//controlla se la casella selezionata in questo momento è uguale a una solo casella della combinazione corretta
+				if(arrayx[i] == comGiuste[c][a]){
+					var casellaEsatta = true
+				}
+			}
+			// se la casella è stata trovata nella combinazione corretta aggiungo uno alla variabile caselle esatte (se raggiunge tre caselle corrette allora è vittoria)
+			if(casellaEsatta == true){
+				caselleEsatte++
+				casellaEsatta = false
+			}
+		}
+		if(caselleEsatte == 3){
+			/*VITTORIA PER LE X*/
+			getElementById("setText").innerText = "ciao";
+		}
+		caselleEsatte = 0
+	}
 }
