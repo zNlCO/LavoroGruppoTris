cb835e2c2d2ce2be6f7bd083128400bc81c03865
Baddy2904 <battiatomatteo29@gmail.com>
2022-05-13 16:47:17 +0200

    ciao

--- Lavoro_Triss/script/tris.js
+++ Lavoro_Triss/script/tris.js
@@ -11,35 +11,36 @@ const comGiuste = [
 	[2, 4, 6]
   ]
 var arrayx = []
-var arrayy = []
+var arrayo = []
 function assegna(posizione)
 {
 	if(turno%2==0)
 	{
-		document.images[posizione].src= "../imm/x.png"
+		var img = document.images[posizione].src= "../imm/x.png"
 		arrayx.push(posizione)
 	}
 	else
 	{
-		document.images[posizione].src="../imm/o.png"
-		arrayy.push(posizione)
+		var img = document.images[posizione].src="../imm/o.png"
+		arrayo.push(posizione)
 		
 	}
-	turno+=1
-	document.images[posizione].disabled= true;
-	if(turno>5){
+	turno+=1 /* incremento del turno */
+
+	if(turno>4)
+	{
 		checkWin();
 	}
 }
 
-function checkWin(){
-	alert("ciao");
+function checkWin()
+{
 	/*variabile per capire se c'è la casella esatta nella combinazione di vittoria*/
 	var casellaEsatta = false
 	//varibile per capire se raggiunge 3 caselle per "formare" la vittoria
 	var caselleEsatte = 0
 	//cicla le 8 combinazioni di vittoria del tris
-	for(var c = 0; i<8; c++){
+	for(var c = 0; c<8; c++){
 		//cicla le caselle dove c'è una x sopra
 		for(var i = 0; i<arrayx.length; i++){
 			//cicla una combinazione di vittoria 
@@ -58,7 +59,39 @@ function checkWin(){
 		if(caselleEsatte == 3){
 			/*VITTORIA PER LE X*/
 			//getElementById("setText").innerText = "ciao";
-			alert("ciao");
+			
+		}
+		caselleEsatte = 0
+	}
+
+
+	/*variabile per capire se c'è la casella esatta nella combinazione di vittoria*/
+	var casellaEsatta = false
+	//varibile per capire se raggiunge 3 caselle per "formare" la vittoria
+	var caselleEsatte = 0
+	//cicla le 8 combinazioni di vittoria del tris
+	for(var c = 0; c<8; c++){
+		//cicla le caselle dove c'è una x sopra
+		for(var i = 0; i<arrayo.length; i++){
+			//cicla una combinazione di vittoria 
+			for(var a = 0; a<3; a++){
+				//controlla se la casella selezionata in questo momento è uguale a una solo casella della combinazione corretta
+				if(arrayo[i] == comGiuste[c][a]){
+					var casellaEsatta = true
+				}
+			}
+			// se la casella è stata trovata nella combinazione corretta aggiungo uno alla variabile caselle esatte (se raggiunge tre caselle corrette allora è vittoria)
+			if(casellaEsatta == true){
+				caselleEsatte++
+				casellaEsatta = false
+			}
+		}
+		if(caselleEsatte == 3){
+			/*VITTORIA PER LE X*/
+			//getElementById("setText").innerText = "ciao";
+
+			//
+			document.getElementById("setText").innerHTML = "Il *giocatore 2*";
 		}
 		caselleEsatte = 0
 	}
