1d04c431ed94d52656d4eae6491b3aa99b8518ef
Baddy2904 <battiatomatteo29@gmail.com>
2022-05-15 17:12:10 +0200

    bottone gioca ancira

--- Lavoro_Triss/script/mostra.js
+++ Lavoro_Triss/script/mostra.js
@@ -1,11 +1,24 @@
-function mostraImmagine()
+function mostra()
 {
 	//fa comparire il bottone gioca e il triss
 	document.getElementById('board').style.display = "grid";
 	document.getElementById('restartButton').style.display = "block";
-	//disabilita i bottoni (blocca quello che si inserisce)
+	//disabilita gli input (blocca quello che si inserisce)
 	document.getElementById('tfGioc1').disabled = true;
 	document.getElementById('tfGioc2').disabled = true;
 	//fa scomparire il bottone:GIOCA
 	document.getElementById('btnImg').style.display = "none";
+}
+
+function mostra2()
+{
+	//fa scomparire il bottone gioca e il triss
+	document.getElementById('board').style.display = "none";
+	document.getElementById('restartButton').style.display = "none";
+	
+	//riabilita gli input
+	document.getElementById('tfGioc1').disabled = false;
+	document.getElementById('tfGioc2').disabled = false;
+	//fa comparire il bottone:GIOCA
+	document.getElementById('btnImg').style.display = "grid";
 }
