fabbb1da56fffeccc7875afa4b7195e649b191c4
Nico <nik04paolini@gmail.com>
2022-05-17 18:05:31 +0200

    TRIS FUNZIONANTE DIO

--- Lavoro_Triss/script/mostra.js
+++ Lavoro_Triss/script/mostra.js
@@ -12,6 +12,12 @@ function mostra()
 
 function mostra2()
 {
+	turno = 0
+	arrayo = []
+	arrayx = []
+	//resetto il tris data (16/05/2022 ora 18:55)
+	cleanTris()
+
 	//fa scomparire il bottone gioca e il triss
 	document.getElementById('board').style.display = "none";
 	document.getElementById('restartButton').style.display = "none";
@@ -21,4 +27,19 @@ function mostra2()
 	document.getElementById('tfGioc2').disabled = false;
 	//fa comparire il bottone:GIOCA
 	document.getElementById('btnImg').style.display = "grid";
+
+	//reset della scritta del vincitore
+	document.getElementById("setText").innerHTML = ""
+
+	
+
+
+}
+
+
+function cleanTris(){
+	for(let i = 0; i<9; i++){
+		document.images[i].src = "../imm/vuoto.jpg"
+	}
+	
 }
--- Lavoro_Triss/script/tris.js
+++ Lavoro_Triss/script/tris.js
@@ -13,40 +13,42 @@ var arrayx = []
 var arrayo = []
 
 var finePartita = true;
+
 function assegna(posizione) {
 
 	let casellaMaiUsata = true
 	for (let b = 0; b < (arrayo.length + arrayx.length); b++) {
 		let arrayxo = arrayo.concat(arrayx)
-		console.log(posizione + " = " + arrayxo[b])
 		if (posizione == arrayxo[b]) {
 			casellaMaiUsata = false
 		}
 	}
 
-	console.log(casellaMaiUsata && finePartita)
 	if (casellaMaiUsata) {
 		if (turno % 2 == 0) {
 			document.images[posizione].src = "../imm/x.png"
 			arrayx.push(posizione)
+			console.log(posizione+ "x")
 		}
 		else {
 			document.images[posizione].src = "../imm/o.png"
 			arrayo.push(posizione)
+			console.log(posizione+ "o")
 		}
-		turno += 1 /* incremento del turno */
-		if (turno > 4) {
-			checkWin();
 		}
+	turno += 1 /* incremento del turno */
+	if (turno > 4) {
+		checkWin();
 	}
 }
 
+
 function checkWin() {
-	/*variabile per capire se c'è la casella esatta nella combinazione di vittoria*/
-	var casellaEsatta = false
+
 	//varibile per capire se raggiunge 3 caselle per "formare" la vittoria
 	var caselleEsatte = 0
 	//cicla le 8 combinazioni di vittoria del tris
+	
 	for (var c = 0; c < 8; c++) {
 		//cicla le caselle dove c'è una x sopra
 		for (var i = 0; i < arrayx.length; i++) {
@@ -54,36 +56,32 @@ function checkWin() {
 			for (var a = 0; a < 3; a++) {
 				//controlla se la casella selezionata in questo momento è uguale a una solo casella della combinazione corretta
 				if (arrayx[i] == comGiuste[c][a]) {
-					var casellaEsatta = true
+					caselleEsatte++
+					
 				}
 			}
-			// se la casella è stata trovata nella combinazione corretta aggiungo uno alla variabile caselle esatte (se raggiunge tre caselle corrette allora è vittoria)
-			if (casellaEsatta == true) {
-				caselleEsatte++
-				casellaEsatta = false
-			}
 		}
+
+		
 		if (caselleEsatte == 3) {
 			finePartita = false
 			/*VITTORIA PER LE X*/
-			//getElementById("setText").innerText = "ciao";
-			console.log(document.getElementById(tfGioc1));
-			if (document.getElementById(tfGioc1) == null) 
-			{
-				document.getElementById("setText").innerHTML =  "Giocatore 1 ha vinto"
+			if (document.getElementById(tfGioc1) == null) {
+				document.getElementById("setText").innerHTML = "Giocatore 1 ha vinto"
 			}
 			else {
 				document.getElementById("setText").innerHTML = document.getElementById(tfGioc1) + " ha vinto"
 			}
+			break
 		}
-		caselleEsatte = 0
+		else{
+			caselleEsatte = 0
+		}
 	}
 
 
-	/*variabile per capire se c'è la casella esatta nella combinazione di vittoria*/
-	var casellaEsatta = false
 	//varibile per capire se raggiunge 3 caselle per "formare" la vittoria
-	var caselleEsatte = 0
+	caselleEsatte = 0
 	//cicla le 8 combinazioni di vittoria del tris
 	for (var c = 0; c < 8; c++) {
 		//cicla le caselle dove c'è una x sopra
@@ -92,28 +90,27 @@ function checkWin() {
 			for (var a = 0; a < 3; a++) {
 				//controlla se la casella selezionata in questo momento è uguale a una solo casella della combinazione corretta
 				if (arrayo[i] == comGiuste[c][a]) {
-					var casellaEsatta = true
+					caselleEsatte++
 				}
 			}
-			// se la casella è stata trovata nella combinazione corretta aggiungo uno alla variabile caselle esatte (se raggiunge tre caselle corrette allora è vittoria)
-			if (casellaEsatta == true) {
-				caselleEsatte++
-				casellaEsatta = false
-			}
 		}
+
+		
 		if (caselleEsatte == 3) {
 			finePartita = false
-			/*VITTORIA PER LE X*/
-			//getElementById("setText").innerText = "ciao";
-			if (document.getElementById(tfGioc2) == null) 
-			{
-				document.getElementById("setText").innerHTML = "Giocatore 1 ha vinto"
+			/*VITTORIA PER LE O*/
+			if (document.getElementById(tfGioc2) == null) {
+				document.getElementById("setText").innerHTML = "Giocatore 2 ha vinto"
 			}
 			else {
 				document.getElementById("setText").innerHTML = document.getElementById(tfGioc2) + " ha vinto"
 			}
+			break
 
 		}
-		caselleEsatte = 0
+		else{
+			caselleEsatte = 0
+		}
 	}
 }
+
